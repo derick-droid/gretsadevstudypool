977bb3d1eca7af5221b98454e8ab9c5cf38f6dda
derick-droid <derrickokinda9@gmail.com>
2022-06-10 21:23:03 +0300

    django inheritance

--- base/views.py
+++ base/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# creating rooms
+rooms = [
+    {'id':1, 'name':'let us learn python'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'frontend developers'},
+
+]
+
 def home(request):
-    return render (request, 'base/home.html')
+    return render (request, 'base/home.html',{'rooms':rooms})
 
 def room(request):
     return render (request, 'base/room.html')
