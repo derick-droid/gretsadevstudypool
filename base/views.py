ac47ba24ec37abd7fe5dc86767f46febd6723aee
derick-droid <derrickokinda9@gmail.com>
2022-06-11 12:48:04 +0300

    creating templates and urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -12,7 +13,14 @@ rooms = [
 ]
 
 def home(request):
-    return render (request, 'base/home.html',{'rooms':rooms})
+    context = {"rooms":rooms}
+    return render (request, 'base/home.html',context)
 
-def room(request):
-    return render (request, 'base/room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+            
+    context = {'room':room}
+    return render (request, 'base/room.html', context)
