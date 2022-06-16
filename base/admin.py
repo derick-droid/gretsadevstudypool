bf770d63c4d30d9f89083d6475533a1dd04ca91a
derick-droid <derrickokinda9@gmail.com>
2022-06-16 11:53:01 +0300

    database model for Topic, messages and User

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, Topic, Messages
 
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Messages)
--- base/models.py
+++ base/models.py
@@ -1,9 +1,21 @@
-# from unicodedata import name
+# from tkinter import CASCADE
+from unicodedata import name
+from functools import update_wrapper
 from django.db import models
+from django.contrib.auth.models import User
+# from base.views import room
 
+class Topic(models.Model):
+    name  = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+
+# room model
 class Room(models.Model):
-    # host = models.CharField(max_length=200)
-    # topic  = models.CharField(max_length=200)
+    host =models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
+    topic  = models.ForeignKey(Topic, on_delete= models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants = models.charfield(max_lenght = 200)
@@ -13,3 +25,14 @@ class Room(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Messages(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    room = models.ForeignKey(Room, on_delete= models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body[0:50]
+    
