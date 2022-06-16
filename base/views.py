3099bc09b5044e29810aff5648e4d9eb30ce4b9b
derick-droid <derrickokinda9@gmail.com>
2022-06-16 12:19:24 +0300

    createing function to help creating new rooms

--- base/views.py
+++ base/views.py
@@ -23,3 +23,7 @@ def room(request, pk):
     room = Room.objects.get(id = pk)
     context = {'room':room}
     return render (request, 'base/room.html', context)
+
+def create_room(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
