3a708f8aed8dbaf1ccaca80d35c3bf0cc47aa3dd
derick-droid <derrickokinda9@gmail.com>
2022-06-21 16:43:34 +0300

    analyzing filter

--- base/views.py
+++ base/views.py
@@ -17,7 +17,8 @@ from .forms import RoomForm
 
 # home code 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains= q)
     topics = Topic.objects.all()
     context = {"rooms":rooms, "topics":topics}
     return render (request, 'base/home.html',context)
