7f99e7b884c5f5632a71ed292e93dfed4cd8fd4e
derick-droid <derrickokinda9@gmail.com>
2022-06-21 17:04:10 +0300

    creating search functionality

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from multiprocessing import context
+from unicodedata import name
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -18,7 +20,10 @@ from .forms import RoomForm
 # home code 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains= q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
+    
+                               
+                                
     topics = Topic.objects.all()
     context = {"rooms":rooms, "topics":topics}
     return render (request, 'base/home.html',context)
