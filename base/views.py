650c1498d7035bbe956a0e7df2f1269eb30d5487
derick-droid <derrickokinda9@gmail.com>
2022-07-04 09:48:54 +0300

    matching room activity and topic

--- base/views.py
+++ base/views.py
@@ -87,7 +87,7 @@ def home(request):
                                 
     topics = Topic.objects.all()
     room_count = rooms.count() # showing number of rooms available 
-    room_messages = Messages.objects.all()
+    room_messages = Messages.objects.filter(Q(room__topic__name__icontains = q))
     
     context = {
                "rooms":rooms,
