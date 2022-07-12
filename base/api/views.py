71526bb787b18d1a217475c20588794ff11fb01a
derick-droid <derrickokinda9@gmail.com>
2022-07-12 21:34:30 +0300

    simple jsoneResponse

--- base/api/views.py
+++ base/api/views.py
@@ -4,8 +4,9 @@ from django.http import JsonResponse
 def getRoutes(request):
     
     routes = [
-        'GET api/rooms',
-        'GET api/rooms:id'
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms:id'
     ]
     
     return JsonResponse(routes, safe=False)
