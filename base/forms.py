bfb0fe983f9e635bb8b266f60da81686e49ae9fc
derick-droid <derrickokinda9@gmail.com>
2022-07-14 22:44:02 +0300

    image attribute

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,6 @@
 from pyexpat import model
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, User
 
 
@@ -13,5 +14,5 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["avatar","name","username", "email", "bio"]
         
