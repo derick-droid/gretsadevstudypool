fc32baaf3d3957a60f2f05364c81ef58b3d3aaed
derick-droid <derrickokinda9@gmail.com>
2022-07-14 21:58:35 +0300

    images field

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 # from tkinter import CASCADE
+from email.policy import default
 from unicodedata import name
 from functools import update_wrapper
 from django.db import models
@@ -8,7 +9,7 @@ class User(AbstractUser):
    name = models.CharField(max_length=200, null=True)
    email = models.EmailField(unique = True, null=True)
    bio = models.TextField(null=True)
-#    avatar = 
+   avatar = models.ImageField(null  = True, default = "avatar.svg")
    USERNAME_FIELD = 'email'
    REQUIRED_FIELDS = []
    
