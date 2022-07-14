e8ae45d8844efdc6ad88fb797b7dcd4a3d5ef25d
derick-droid <derrickokinda9@gmail.com>
2022-07-14 21:12:28 +0300

    comment out user model

--- base/models.py
+++ base/models.py
@@ -5,43 +5,43 @@ from django.db import models
 from django.contrib.auth.models import User
 # from base.views import room
 
-class Topic(models.Model):
-    name  = models.CharField(max_length=200)
+# class Topic(models.Model):
+#     name  = models.CharField(max_length=200)
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
-# room model
-class Room(models.Model):
-    host =models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
-    topic  = models.ForeignKey(Topic, on_delete= models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name="participants", blank=True)
-    update = models.DateTimeField(auto_now = True)
-    created_date = models.DateTimeField(auto_now_add = True)
+# # room model
+# class Room(models.Model):
+#     host =models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
+#     topic  = models.ForeignKey(Topic, on_delete= models.SET_NULL, null=True)
+#     name = models.CharField(max_length=200)
+#     description = models.TextField(null=True, blank=True)
+#     participants = models.ManyToManyField(User, related_name="participants", blank=True)
+#     update = models.DateTimeField(auto_now = True)
+#     created_date = models.DateTimeField(auto_now_add = True)
     
-    # to show the rooms created in asecending order
-    class Meta:
-        ordering = ["-update", "-created_date"]
+#     # to show the rooms created in asecending order
+#     class Meta:
+#         ordering = ["-update", "-created_date"]
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
     
-    # messages to be sent
-class Messages(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    room = models.ForeignKey(Room, on_delete= models.CASCADE)
-    body = models.TextField()
-    update = models.DateTimeField(auto_now=True)
-    created_date = models.DateTimeField(auto_now_add=True)
+#     # messages to be sent
+# class Messages(models.Model):
+#     user = models.ForeignKey(User, on_delete= models.CASCADE)
+#     room = models.ForeignKey(Room, on_delete= models.CASCADE)
+#     body = models.TextField()
+#     update = models.DateTimeField(auto_now=True)
+#     created_date = models.DateTimeField(auto_now_add=True)
      
-    # to show the rooms created in asecending order
-    class Meta:
-        ordering = ["-update", "-created_date"]
+#     # to show the rooms created in asecending order
+#     class Meta:
+#         ordering = ["-update", "-created_date"]
     
     
-    def __str__(self):
-        return self.body[0:50]
+#     def __str__(self):
+#         return self.body[0:50]
     
